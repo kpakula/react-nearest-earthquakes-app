23d5ec6fb236e448e14188cacd241f9cf670e554
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-01-19 13:09:08 +0100

    Created dynamic make a rows

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
+import RandomComponent from "./components/testComponents/RandomComponent";
 
 function App() {
   return (
       <Container fluid={true} className="App">
-        <Row>
+        {/* <Row>
           <Col className="border">1 of 2</Col>
           <Col className="border">2 of 2</Col>
         </Row>
@@ -14,7 +15,8 @@ function App() {
           <Col className="border">1 of 3</Col>
           <Col className="border">2 of 3</Col>
           <Col className="border">3 of 3</Col>
-        </Row>
+        </Row> */}
+        <RandomComponent/>
       </Container>
   );
 }
