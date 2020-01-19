4880aaa304e953f030378a5ec8650cda21d3097d
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-01-19 09:14:18 +0100

    Added necessary dependencies - bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { PositionDemo } from './components/Position/PositionDemo';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './components/Main/Main';
 
 
