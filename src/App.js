573de8488f5e758aa386a0db89dc9980987a7852
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-01-19 08:51:15 +0100

    Refactored main view and add some extra instructions with styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { PositionDemo } from './components/Position/PositionDemo';
 import './App.css';
+import Main from './components/Main/Main';
 
 
 function App() {
   return (
     <div className="App">
+      <Main/>
       {/* <PositionDemo /> */}
     </div>
   );
---
+++ src/components/Main/Main.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import './Main.css';
+
+
+export default class Main extends Component {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // Main view
+    render() {
+        return (
+            <div className="container">
+                <h1>Get your location...</h1>
+            </div>
+        )
+    }
+}
+
