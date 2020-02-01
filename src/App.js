9810918044591582bdcc1c39c28f7161f9930e7a
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-02-01 19:15:22 +0100

    Updated current map view

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+// import { Container } from "react-bootstrap";
 import { Container, Col, Row } from "react-bootstrap";
-import { CurrentGeoLocation } from "./components/Position/CurrentGeoLocation";
+// import { CurrentGeoLocation } from "./components/Position/CurrentGeoLocation";
+import { MapView } from "./components/Map/Map";
 
 function App() {
   return (
     <Container fluid={true} className="App">
-      <CurrentGeoLocation />
+      {/* <CurrentGeoLocation> */}
+
+
+        {/* </CurrentGeoLocation> */}
       <Row>
-        <Col></Col>
+        <Col className="align-self-center">
+          <MapView/>
+        </Col>
+{/* 
+        <Col>
+          sranie
+        </Col> */}
       </Row>
     </Container>
   );
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react'
+import React from "react";
+import L from "leaflet";
 
-export default class Map extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+
+import './Map.css'
+ export class MapView extends React.Component {
+  
+    componentDidMount() {
+    this.map = L.map('map', {
+      center: [49.8419, 24.0315],
+      zoom: 2,
+      layers: [
+        L.tileLayer(
+          "https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=bm3pLwuFHdjGvaCardpP",
+          {
+            attribution:
+              '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+          }
+        ),
+      ]
+    });
+
+    this.marker = L.marker([50.5, 30.5]).addTo(this.map)
+  }
+
+  render() {
+    return <div id="map"></div>;
+  }
 }
