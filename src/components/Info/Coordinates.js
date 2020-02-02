95c3a6d8385d441a01190cf2e5632bffb430c62d
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-02-02 15:36:10 +0100

    Show your latitude and longitude

--- src/components/Info/Coordinates.js
+++ src/components/Info/Coordinates.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 
-
 export default function Coordinates(props) {
+  let showLatitude;
+  let showLongitude;
+
+  if (props.latitude && props.longitude) {
+    showLatitude = <b>Latitude</b>;
+    showLongitude = <b>Longitude</b>;
+  }
+
   return (
     <Row className="mt-3">
       <Col>
+        {showLatitude}
         <h3>{props.latitude}</h3>
       </Col>
       <Col>
+        {showLongitude}
         <h3>{props.longitude}</h3>
       </Col>
     </Row>
