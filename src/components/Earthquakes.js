0a8b33cd9f88eb28ba572da7405a82a15883e82f
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-03-19 15:08:04 +0100

    Fixed date for specific earthquake

--- src/components/Earthquakes.js
+++ src/components/Earthquakes.js
@@ -3,25 +3,18 @@ import React from "react";
 import "./Earthquakes.css";
 import moment from "moment";
 function Earthquakes({ topEarthquakes }) {
-  const day = moment(topEarthquakes.date).format("YYYY-MM-DD HH:mm:ss")
-
   const earthquakes = topEarthquakes.map((earthquake, index) => (
     <div className="earthquake" key={index}>
-        <p>{index + 1}.</p>
-        <p>{earthquake.title} </p>
-        <p>Lat: {earthquake.latitude}</p>
-        <p>Long: {earthquake.longitude}</p>
-        <p>Kilometers: {earthquake.kilometers}</p>
-        <p>Date: {day}</p>
-
+      <p>{index + 1}.</p>
+      <p>{earthquake.title} </p>
+      <p>Lat: {earthquake.latitude}</p>
+      <p>Long: {earthquake.longitude}</p>
+      <p>Kilometers: {earthquake.kilometers}</p>
+      <p>Date: {moment(earthquake.date).format("YYYY-MM-DD HH:mm:ss")}</p>
     </div>
   ));
 
-  return (
-    <div className="earthquakes">
-      {earthquakes}
-    </div>
-  );
+  return <div className="earthquakes">{earthquakes}</div>;
 }
 
 export default Earthquakes;
