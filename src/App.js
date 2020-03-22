eca038359a297207d684aa7721e048ca3202795c
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-03-22 12:31:08 +0100

    Refactored current picked earthquake

--- src/App.js
+++ src/App.js
@@ -13,18 +13,22 @@ import Earthquakes from "./components/Earthquakes";
 import { EARTHQUAKE_API } from "./utils/Api";
 import CustomModal from "./components/Modal/CustomModal";
 import CheckButton from "./components/Buttons/CheckButton";
+import getRandomInRange from "./utils/Random"
 
 function App() {
   const [markers, setMarkers] = useState([]);
   const [currentLatitude, setCurrentLatitude] = useState(null);
   const [currentLongitude, setCurrentLongitude] = useState(null);
-  const [isResponse, setResponse] = useState(undefined);
+  const [isLoadingBar, setLoadingBar] = useState(undefined);
   const [topEarthquakes, setTopEarthquakes] = useState([]);
   const [isShowModal, setShowModal] = useState(false);
 
-  const handleClose = () => setShowModal(false);
 
-  const handleRandom = () => {
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
+
+  const getRandomCoordinates = () => {
     const randomLat = getRandomInRange(-90, 90).toFixed(6);
     const randomLong = getRandomInRange(-180, 180).toFixed(6);
 
@@ -34,14 +38,14 @@ function App() {
 
     setTopEarthquakes([]);
   };
-  const handleShow = () => setShowModal(true);
 
-  const getRandomInRange = (min, max) => {
-    return Math.random() * (max - min) + min;
-  };
 
-  const request = async () => {
-    setResponse(true);
+
+
+
+  const makeEarthquakesRequest = async () => {
+
+    setLoadingBar(true);
     setTopEarthquakes([]);
     try {
       const response = await axios.get(EARTHQUAKE_API);
@@ -58,7 +62,7 @@ function App() {
     } catch (error) {
       console.error(error);
     } finally {
-      setResponse(false);
+      setLoadingBar(false);
     }
   };
 
@@ -125,42 +129,7 @@ function App() {
     return earthquakes;
   };
 
-  const handleCurrentPickedEarthquake = event => {
-    const id = event.currentTarget.dataset.id;
 
-    const updatedEarthquakes = [...topEarthquakes];
-
-    updatedEarthquakes.forEach((earthquake, index) => {
-      if (index !== id) {
-        earthquake.clicked = false;
-      }
-    });
-
-    updatedEarthquakes[id].clicked = true;
-
-    if (markers.length === 1) {
-      setMarkers([
-        ...markers,
-        {
-          latitude: updatedEarthquakes[id].latitude,
-          longitude: updatedEarthquakes[id].longitude,
-          kilometers: updatedEarthquakes[id].kilometers,
-          title: updatedEarthquakes[id].title,
-          date: updatedEarthquakes[id].date
-        }
-      ]);
-    } else {
-      const updateMarkers = [...markers];
-      updateMarkers[1].latitude = updatedEarthquakes[id].latitude;
-      updateMarkers[1].longitude = updatedEarthquakes[id].longitude;
-      updateMarkers[1].kilometers = updatedEarthquakes[id].kilometers;
-      updateMarkers[1].title = updatedEarthquakes[id].title;
-      updateMarkers[1].date = updatedEarthquakes[id].date;
-      setMarkers(updateMarkers);
-    }
-
-    setTopEarthquakes(updatedEarthquakes);
-  };
 
   return (
     <Container fluid={true} className="App">
@@ -179,16 +148,18 @@ function App() {
               handleShow={handleShow}
             />
 
-            <CheckButton request={request} />
+            <CheckButton request={makeEarthquakesRequest} />
 
             <div className="row justify-content-center align-items-center flex-grow-1">
               {topEarthquakes.length > 0 && (
                 <Earthquakes
                   topEarthquakes={topEarthquakes}
-                  handleCurrentPickedEarthquake={handleCurrentPickedEarthquake}
+                  markers={markers}
+                  setMarkers={setMarkers}
+                  setTopEarthquakes={setTopEarthquakes}
                 />
               )}
-              {isResponse && <ReactLoading type="bars" />}
+              {isLoadingBar && <ReactLoading type="bars" />}
             </div>
           </div>
         </div>
@@ -196,7 +167,7 @@ function App() {
       <CustomModal
         isShowModal={isShowModal}
         handleClose={handleClose}
-        handleRandom={handleRandom}
+        handleRandom={getRandomCoordinates}
         currentLatitude={currentLatitude}
         currentLongitude={currentLongitude}
       />
--- src/components/Earthquakes.js
+++ src/components/Earthquakes.js
@@ -1,7 +1,51 @@
 import React from "react";
 
 import "./Earthquakes.css";
-function Earthquakes({ topEarthquakes, handleCurrentPickedEarthquake }) {
+function Earthquakes({ topEarthquakes, setTopEarthquakes, markers, setMarkers }) {
+
+  const handleCurrentPickedEarthquake = event => {
+    const id = event.currentTarget.dataset.id;
+    const updatedEarthquakes = [...topEarthquakes];
+    
+    updatedEarthquakes.forEach((earthquake, index) => {
+      if (index !== id) {
+        earthquake.clicked = false;
+      }
+    });
+
+    updatedEarthquakes[id].clicked = true;
+
+    if (markers.length === 1) {
+      setMarkers([
+        ...markers,
+        {
+          latitude: updatedEarthquakes[id].latitude,
+          longitude: updatedEarthquakes[id].longitude,
+          kilometers: updatedEarthquakes[id].kilometers,
+          title: updatedEarthquakes[id].title,
+          date: updatedEarthquakes[id].date
+        }
+      ]);
+    } else {
+      const updateMarkers = [...markers];
+      updateMarkers[1].latitude = updatedEarthquakes[id].latitude;
+      updateMarkers[1].longitude = updatedEarthquakes[id].longitude;
+      updateMarkers[1].kilometers = updatedEarthquakes[id].kilometers;
+      updateMarkers[1].title = updatedEarthquakes[id].title;
+      updateMarkers[1].date = updatedEarthquakes[id].date;
+      setMarkers(updateMarkers);
+    }
+
+    setTopEarthquakes(updatedEarthquakes);
+  };
+
+
+
+
+
+
+
+
   const earthquakes = topEarthquakes.map((earthquake, index) => (
     <div
       className={
